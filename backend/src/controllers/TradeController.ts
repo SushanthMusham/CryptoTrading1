1fe1c763c4966a367a3764b3608885cd712e79d5
DORAEMONlovesMICHAN <mushamsushanth@gmail.com>
2026-03-22 15:07:46 +0530

    fix: force nodemailer to use ipv4 smtp to bypass render block

--- backend/src/controllers/TradeController.ts
+++ backend/src/controllers/TradeController.ts
@@ -50,7 +50,9 @@ export class TradeController {
             
             if (user && user.email) {
                 const transporter = nodemailer.createTransport({
-                    service: 'gmail',
+                    host: 'smtp.gmail.com',
+                    port: 465,
+                    secure: true,
                     auth: {
                         user: process.env.EMAIL_USER,
                         pass: process.env.EMAIL_PASS
